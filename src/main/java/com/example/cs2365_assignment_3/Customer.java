af7055dd39358678f3a8145dc9a1808a63b17890
jcala09 <61167047+jcala09@users.noreply.github.com>
2021-12-01 19:05:01 -0600

    Update Customer.java

--- src/main/java/com/example/cs2365_assignment_3/Customer.java
+++ src/main/java/com/example/cs2365_assignment_3/Customer.java
@@ -5,22 +5,98 @@ public class Customer {
 
     private String firstName;
     private String lastName;
-    private int rewardNumber;
+    private String rewardNumber;
     private String eliteStatus;
-    private double discountPercentage;  // For Elite customers
-    private int starsEarned;            // For regular customers
+    private String discountPercentage;  // For Elite customers
+    private String email; 
+    private String phoneNum;
 
-    public void getFirstName(){
-
+    //getters and setters
+    public String getFirstName(){
+        return this.firstName;
     }
-    public void setFirstName(){
-
+    public void setFirstName(String fname){
+        firstName= fname; 
     }
-    public void getLastName(){
-
+    public String getLastName(){
+        return this.lastName;
     }
-    public void setLastName(){
-
+    public void setLastName(String fname){
+        lastName= fname; 
+    }
+    public String getrewardNumber(){
+        return this.rewardNumber;
+    }
+    public void setrewardNumber(String fname){
+        rewardNumber= fname; 
+    }
+    public String geteliteStatus(){
+        return this.eliteStatus;
+    }
+    public void seteliteStatus(String fname){
+        eliteStatus= fname; 
+    }
+    public String getdiscountPercentage(){
+        return this.discountPercentage;
+    }
+    public void setdiscountPercentage(String fname){
+        discountPercentage= fname; 
+    }
+    public String getemail(){
+        return this.email;
+    }
+    public void setemail(String fname){
+        email= fname; 
+    }
+    public String getphoneNum(){
+        return this.email;
+    }
+    public void setphoneNum(String fname){
+        email= fname; 
     }
 
+ /** NAME: customer
+ PARAMETERS: none
+ PURPOSE: default constructor
+ PRECONDITION: variables declared
+ POSTCONDITION: all variables have defaults
+*/   
+    public Customer()
+	{
+		this.firstName= "FIRSTNAME";
+		this.lastName= "LASTNAME";
+		this.rewardNumber= "NULL";
+		this.eliteStatus= "NA";
+		this.email="firstname.lastname@rNum.company.com";
+		this.phoneNum="123-456-7890";
+	}
+
+	
+/** NAME: customer
+ PARAMETERS: String fname, String lname, String rNum, String email
+ PURPOSE: assign values to variables
+ PRECONDITION: none
+ POSTCONDITION: no return, just variables assigned
+*/
+	public Customer(String fname, String lname, String rNum, String eStatus, String Email, String pNum)
+	{
+		this.firstName=fname;
+		this.lastName= lname;
+		this.rewardNumber= rNum;
+		this.email= Email;
+		this.eliteStatus= eStatus;
+		this.phoneNum= pNum;
+	}
+
+
+		/** NAME: toString
+ PARAMETERS: none
+ PURPOSE: return string
+ PRECONDITION: none
+ POSTCONDITION: returning string for object
+*/
+	public String toString()
+	{
+		return ("Name:\t\t\t\t" + firstName + " " + lastName + "\nrNum:\t\t\t" + rewardNumber + "\nElite Status:\t\t"+ eliteStatus+ "\nemail:\t" + email + "\nphone num:\t\t\t" + phoneNum );
+	}
 }
